3f2bb636afa235f2577f51a51e4dd51da99b2b1d
guilherme santos <guilhermess1994@gmail.com>
2020-09-22 10:46:28 -0300

    fixed margins

--- src/components/FamilyAdd.vue
+++ src/components/FamilyAdd.vue
@@ -11,50 +11,71 @@ div
   q-dialog(v-model="dialog", persistent, position="bottom")
     q-card.bg-primary.text-white
       q-card-section
-        .text-h6(Adicione, uma, nova, familia)
+        .text-h6 Adicione uma nova familia
 
-      q-card-section(style="min-height: 300px")
-        q-form
-          q-input.q-mb-md(
+      q-card-section(style="min-height: 450px")
+        q-input.q-mb-md(
             label="Nome da familia",
             standout="bg-white text-primary",
             dark,
             dense,
             v-model="familyName"
-          )
+        )
 
-          q-separator.q-mt-md.q-mb-md
-
-          .row.q-gutter-md
-            .col
-              q-input(
+        q-separator.q-mt-md.q-mb-md
+        p.text-grey Adicione membros a essa familia
+        .row.q-gutter-md
+          .col-5
+            q-input(
                 label="Nome",
                 standout="bg-white text-primary",
                 dark,
                 dense,
                 v-model="membersToAdd.name"
               )
-            .col
-              q-input(
+          .col-5
+            q-input(
                 label="telefone",
                 standout="bg-white text-primary",
                 dark,
+                tyle="tel"
                 dense,
+                mask="(##) # ####-####",
                 v-model="membersToAdd.telephone"
-              )
-            .col.text-black(
-              q-btn,
-              push,
-              color="white",
-              icon="mdi-plus",
-              round,
-              dense
             )
-            q-separator.q-mt-md.q-mb-md
+          .col
+            q-btn(
+                push,
+                color="white",
+                class="text-black"
+                icon="mdi-plus",
+                round,
+                dense,
+                @click="addMember()"
+              )
+          q-separator.q-mt-md.q-mb-md
+
+          q-card(style="width: 100%" class="text-black")
+            q-card-section
+              p.text-grey Membros adicionados
+
+              .row.flex.flex-center
+                q-icon(v-show="members.length < 1" style="font-size: 5em;" class="text-grey" name="mdi-card-bulleted-off-outline")
+
+              q-list(clickable separator style="width: 100%")
+                q-item(v-for="(member, index) in members" :key="index")
+                  q-item-section(avatar)
+                    q-icon(color="primary" name="mdi-account-group")
+                  q-item-section
+                    q-item-label {{ member.name }}
+                    q-item-label(caption) {{ member.telephone }}
+                  q-item-section(side)
+                    q-btn(icon="mdi-close" flat round @click="removeMember(index)")
 
       q-card-actions.bg-primary.text-white(align="between")
         q-btn(flat, label="fechar", v-close-popup)
-        q-btn.text-black(
+        q-btn(
+          class="text-black"
           push,
           color="white",
           icon="mdi-plus",
@@ -72,7 +93,21 @@ export default {
     membersToAdd: {
       name: "",
       telephone: ""
+    },
+    members: []
+  }),
+
+  methods: {
+    addMember() {
+      console.log("add");
+      this.members.push({ ...this.membersToAdd });
+      this.membersToAdd.name = "";
+      this.membersToAdd.telephone = "";
+    },
+
+    removeMember(index) {
+      this.members.splice(index, 1);
     }
-  })
+  }
 };
 </script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   q-page
-    p( class="text-h4 text-grey font-title") Familias
+    p( class=" q-ma-md text-h4 text-grey font-title") Familias
     p( class="flex flex-center")
       family-add
     family-list( :families="FAMILIES")
